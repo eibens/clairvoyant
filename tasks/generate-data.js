86454dc3d94f5596efd9681c6929c2e3709145cb
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2017-02-22 04:03:15 +0100

    Update 2017-02-22T03:02:13.323Z

--- tasks/generate-data.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var numberman = require('../tools/numberman.js');
-
-// Generate the Worm data
-gulp.task('generate-data', function() {
-  var data = numberman('data/worm.json', 'data/patterns.txt');
-  fs.writeFileSync('app/data.json', data);
-});
